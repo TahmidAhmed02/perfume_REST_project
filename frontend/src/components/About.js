9e016776bb85ab72f725a55dfa6b44c8039c7927
Tahmid <tahmidahmed_02@outlook.com>
2024-12-04 19:10:23 +0000

    Added more CSS and fixed some sizing issues

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,23 +7,26 @@ export default function About() {
       <section className="aboutSection">
         <h1 className="title">ABOUT US</h1>
           <p className="para">
-             Welcome to <strong>Dubai Aroma</strong>, your premier destination for high-quality clothing and exquisite perfumes. We pride ourselves on serving both retail and wholesale customers with a wide range of products that cater to modesty, elegance, and authenticity. Our mission is to provide clothing and fragrances that embraces timeless fashion and luxury.
-          </p>
+             Welcome to <strong>Dubai Aroma</strong>,Welcome to Dubai Aroma, your premier destination for high-quality clothing and exquisite perfumes. We pride ourselves on serving both retail and wholesale customers with a wide range of products that cater to modesty, elegance, and authenticity. Our mission is to provide clothing and fragrances that embrace timeless fashion and luxury.
+At Dubai Aroma, we combine tradition with innovation, curating a selection that reflects the unique culture and vibrance of Dubai while staying attuned to global trends. Whether you’re looking for modest attire, sophisticated fragrances, or exclusive styles, our collections are designed to inspire confidence and individuality.
+</p>
       </section>
 
       <section className="aboutVision">
         <h2 className="title">Our Vision</h2>
         <p className="para">
-          At <strong>Dubai Aroma</strong>, our vision is to provide the best clothing and fragrances available on the market for individuals and businesses
+          At <strong>Dubai Aroma</strong>,Our vision is to create a haven of elegance and individuality where our customers can discover the perfect expression of their style and personality. By offering an exquisite collection of perfumes and clothing, we aim to blend timeless sophistication with modern trends, ensuring every visit is an inspiring journey. We are committed to fostering a sense of confidence and uniqueness in our customers, empowering them to embrace their essence with every purchase. At the heart of our vision is a dedication to quality, sustainability, and exceptional service, crafting a shopping experience that is as luxurious as the products we offer.
         </p>
       </section>
 
       <section className="aboutValues">
-        <h2 className="values">Our Values</h2>
+        <h2 className="title">Our Values</h2>
         <ul className="para">
           <li><strong>Quality:</strong> We offer products crafted from the finest materials, ensuring durability and comfort.</li>
           <li><strong>Effeciency:</strong> Our business ensures that there is always someone at hand to help you with your enquiries and deliveries</li>
           <li><strong>Inclusivity:</strong> We celebrate diversity and aim to serve customers of all backgrounds and needs.</li>
+          <li><strong>Sustainability:</strong> We are committed to ethical practices, sourcing eco-friendly materials, and promoting sustainable business practices to reduce our environmental impact.</li>
+          <li><strong>Integrity:</strong>We uphold honesty, transparency, and ethical principles in all aspects of our business, fostering trust and loyalty among our customers and partners.</li>
         </ul>
       </section>
     </div>
--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -3,17 +3,18 @@ import './css/Contact.css'
 
 export default function Contact() {
   return (
-    <div>
+    <div className="img">
       <section className="contactSection">
 
         <h2 id="contactHeader">Contact Us</h2>
 
         <p className="contactPara">If you have any questions, feel free to reach us through the following:</p>
+        <p className="contactPara">(* Not real Contacts *)</p>
 
         <address className="contactContainer">
-          <strong className="contactTitle">Phone:</strong> <a href="tel:+123456789">+44 791 2838813</a>
-          <strong className="contactTitle">Email:</strong> <a href="mailto:info@example.com">dubaiAroma@outlook.com</a>
-          <strong className="contactTitle">Address:</strong> 100 Commercial Road, London, England, E1 1NU
+          <strong className="contactTitle">Phone: <a className="href" href="tel:+123456789">+44 791 2838813</a></strong>
+          <strong className="contactTitle">Email: <a className="href" href="mailto:info@example.com">dubaiAroma@outlook.com</a></strong>
+          <strong className="contactTitle">Address: <span className="href">100 Commercial Road, London, England, E1 1NU</span></strong>
         </address>
 
       </section>
