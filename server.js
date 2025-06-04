67ce082916869bd5837dfecf1a220254eb2b366a
Tahmid <tahmidahmed_02@outlook.com>
2025-06-04 12:19:00 +0100

    updated DB URL and package.json to alllow for upload onto Mongo atlas and Render

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 const mongoose = require('mongoose')
 
 //setting up mongoose
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.MONGODB_URI)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
